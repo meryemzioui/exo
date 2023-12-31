d28e329ddf179f3a2f7390ea4e749a6d27270de1
meryemzioui <153532740+meryemzioui@users.noreply.github.com>
2023-12-31 11:50:09 +0100

    3eme commit

--- exo.js
+++ exo.js
@@ -1,17 +1,34 @@
-var produit=["element1","element2","element3"]
-for (i=0;i<produit.length;i++)
-{
-    console.log(produit[i])
+// var produit=["element1","element2","element3"]
+// for (i=0;i<produit.length;i++)
+// {
+//     console.log(produit[i])
+// }
+
+// // moyenne
+
+// var point=[10,15,10];
+// somme=0;
+// for ( i=0;i<point.length;i++)
+// {
+//      somme=somme + point[i];
+// }
+// var moyenne=somme/point.length
+
+//     console.log("la moyenne est : ",moyenne)
+
+// valeur min
+var point = [10, 15, 11, 5, 0, 20];
+var min = point[0];
+for (i = 1; i < point.length; i++) {
+  if (min > point[i]) {
+    min = point[i];
+  }
 }
-
-// moyenne
-
-var point=[10,15,10];
-somme=0;
-for ( i=0;i<point.length;i++)
-{
-     somme=somme + point[i];
+console.log("valeur minimale : ", min);
+var max = point[0];
+for (i = 1; i < point.length; i++) {
+  if (max < point[i]) {
+    max = point[i];
+  }
 }
-var moyenne=somme/point.length
-
-    console.log("la moyenne est : ",moyenne)
+console.log("valeur mmaximale : ", max);
