529e07ad3ef4df822406e3e4135b2c7c8f3fc466
meryemzioui <153532740+meryemzioui@users.noreply.github.com>
2023-12-31 10:46:35 +0100

    2eme commit

---
+++ exo.js
@@ -0,0 +1,17 @@
+var produit=["element1","element2","element3"]
+for (i=0;i<produit.length;i++)
+{
+    console.log(produit[i])
+}
+
+// moyenne
+
+var point=[10,15,10];
+somme=0;
+for ( i=0;i<point.length;i++)
+{
+     somme=somme + point[i];
+}
+var moyenne=somme/point.length
+
+    console.log("la moyenne est : ",moyenne)
